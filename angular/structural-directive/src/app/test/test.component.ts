822bf6ad7dddf64c669e8a0f67dbf084361900e1
saad-ahmed-onval <saad.ahmed@onval.co>
2019-07-24 10:04:15 +0530

    sadsf

--- angular/structural-directive/src/app/test/test.component.ts
+++ angular/structural-directive/src/app/test/test.component.ts
@@ -3,12 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2 *ngIf="true"> Welcome</h2>
+    <h2 *ngIf='displayName2; else elseBlock1'> Welcome</h2>
 
-  `,
+    <ng-template #elseBlock1>
+      <h2> Name is hidden </h2>
+    </ng-template>
+
+<!--ascs-->
+
+    <div [ngSwitch]="color">
+      <div *ngSwitchCase="'red'">you picked red</div>
+      <div *ngSwitchCase="'green'">you picked green</div>
+      <div *ngSwitchCase="'blue'">you picked blue</div>
+      <div *ngSwitchDefault>Click Again</div>
+
+    </div>
+
+    `,
   styles: []
 })
+
 export class TestComponent implements OnInit {
+
+  public color = 'sadfsv';
+
+
 
   constructor() { }
 
